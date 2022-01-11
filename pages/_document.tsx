224124ecccce43b2689ac1f2a817772455bf20db
Takahiro <tkhr_621@docomo.ne.jp>
2022-01-11 23:59:24 +0900

    色合い修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ const MyDocument = () => {
   return (
     <Html lang="ja-JP">
       <Head>{/* <meta name="application-name" content="MyApp" /> */}</Head>
-      <body className="dark:bg-gray-900">
+      <body className="dark:bg-gray-800">
         <Main />
         <NextScript />
       </body>
