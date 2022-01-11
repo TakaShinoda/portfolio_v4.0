1dbd4d7ab2375d586ab1f5fca8e93fd58a13438f
Takahiro <tkhr_621@docomo.ne.jp>
2022-01-11 23:59:06 +0900

    コメント追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', //ダークモードを有効化
+  darkMode: 'class', //ダークモードを有効化(ON/OFFの切り替え)
   theme: {
     extend: {},
   },
