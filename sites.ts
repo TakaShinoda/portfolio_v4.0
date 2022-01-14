d6fa0f4d4f5f880f7427cc62ef01fef9fbc55c01
Takahiro <tkhr_621@docomo.ne.jp>
2022-01-15 03:12:31 +0900

    取得するサイトのURL

---
+++ sites.ts
@@ -0,0 +1,12 @@
+import { Sites } from './types/types'
+
+export const sites: Sites[] = [
+    {
+      sources: [
+        "https://zenn.dev/taka_shino/feed",
+        "https://takashinoda.hatenablog.com/rss",
+      ],
+      includeUrlRegex: "",
+      excludeUrlRegex: ""
+    },
+]
