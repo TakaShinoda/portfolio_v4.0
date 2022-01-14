9b60ea3419e5077034f214f49bf6d5f0dfd8c2fc
Takahiro <tkhr_621@docomo.ne.jp>
2022-01-15 03:14:42 +0900

    RSS情報取得

---
+++ builder/posts.ts
@@ -0,0 +1,82 @@
+import fs from 'fs-extra'
+import Parser from 'rss-parser'
+import { sites } from '../sites'
+import { Sites, PostItem } from '../types/types'
+export default {}
+
+type FeedItem = {
+    title: string
+    link: string
+    contentSnippet?: string
+    isoDate?: string
+    dateMiliSeconds: number
+}
+
+const parser = new Parser()
+let allPostItems: PostItem[] = []
+
+async function fetchFeedItems(url: string) {
+    const feed = await parser.parseURL(url)
+    if (!feed?.items?.length) return []
+
+    // return item which has title and link
+    return feed.items
+    .map(({ title, contentSnippet, link, isoDate }) => {
+        return {
+          title,
+          contentSnippet: contentSnippet?.replace(/\n/g, ""),
+          link,
+          isoDate,
+          dateMiliSeconds: isoDate ? new Date(isoDate).getTime() : 0,
+        }
+    })
+    .filter(({ title, link }) => title && link) as FeedItem[]
+}
+
+async function getFeedItemsFromSources(sources: undefined | string[]) {
+    if (!sources?.length) return [];
+    let feedItems: FeedItem[] = [];
+    for (const url of sources) {
+      const items = await fetchFeedItems(url);
+      if (items) feedItems = [...feedItems, ...items];
+    }
+    return feedItems;
+}
+
+async function getMemberFeedItems(member: Sites): Promise<PostItem[]> {
+    const { sources, includeUrlRegex, excludeUrlRegex } = member;
+    const feedItems = await getFeedItemsFromSources(sources);
+    if (!feedItems) return [];
+  
+    let postItems = feedItems.map((item) => {
+      return {
+        ...item,
+        // authorName: name,
+        // authorId: id,
+      };
+    });
+    // remove items which not matches includeUrlRegex
+    if (includeUrlRegex) {
+      postItems = postItems.filter((item) => {
+        return item.link.match(new RegExp(includeUrlRegex));
+      });
+    }
+    // remove items which matches excludeUrlRegex
+    if (excludeUrlRegex) {
+      postItems = postItems.filter((item) => {
+        return !item.link.match(new RegExp(excludeUrlRegex));
+      });
+    }
+  
+    return postItems;
+}
+
+(async function () {
+    for (const site of sites) {
+      const items = await getMemberFeedItems(site);
+      if (items) allPostItems = [...allPostItems, ...items];
+    }
+    allPostItems.sort((a, b) => b.dateMiliSeconds - a.dateMiliSeconds);
+    fs.ensureDirSync(".contents");
+    fs.writeJsonSync(".contents/posts.json", allPostItems);
+})();
