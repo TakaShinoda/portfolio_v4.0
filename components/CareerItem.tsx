863835f9fe8f682cafcef9ffb9df8aa4e02354b8
Takahiro <tkhr_621@docomo.ne.jp>
2022-01-19 22:32:52 +0900

    add test

--- components/CareerItem.tsx
+++ components/CareerItem.tsx
@@ -11,11 +11,17 @@ export const CareerItem: VFC<Props> = ({ career, from, to }) => {
     <li className="mb-5">
       <div className="flex items-center mb-1">
         <div className="bg-gray-300 rounded-full h-3 w-3"></div>
-        <div className="flex-1 ml-4 font-medium text-gray-800 dark:text-white">
+        <div
+          className="flex-1 ml-4 font-medium text-gray-800 dark:text-white"
+          data-testid="career"
+        >
           {career}
         </div>
       </div>
-      <div className="ml-7 text-xs font-medium pr-3 my-2 text-gray-500 dark:text-gray-300">
+      <div
+        className="ml-7 text-xs font-medium pr-3 my-2 text-gray-500 dark:text-gray-300"
+        data-testid="from-to"
+      >
         {from} - {to ?? ''}
       </div>
     </li>
