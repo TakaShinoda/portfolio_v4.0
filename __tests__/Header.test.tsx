0c9d871d95d145127dc2e53a9dd1ce6b82781724
Takahiro <tkhr_621@docomo.ne.jp>
2022-09-19 12:57:15 +0900

    next/router をモックする

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { Header } from '../components/layouts/Header'
 
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      locale: 'ja',
+    }
+  },
+}))
+
 describe('表示項目のテスト', () => {
-    test('タイトルが表示されているか', () => {
-        render(<Header />)
-        expect(screen.getByRole('heading')).toHaveTextContent('console.log(me)')
-    })
+  test('タイトルが表示されているか', () => {
+    render(<Header />)
+    expect(screen.getByRole('heading')).toHaveTextContent('console.log(me)')
+  })
 })
