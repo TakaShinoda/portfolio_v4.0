6e280798a75c15084ce80ff51b40c44394b27d5c
Takahiro <tkhr_621@docomo.ne.jp>
2023-02-05 17:06:23 +0900

    chore: add types

--- types/types.ts
+++ types/types.ts
@@ -1,13 +1,20 @@
 export type Sites = {
-    sources?: string[]
-    includeUrlRegex?: string
-    excludeUrlRegex?: string
+  sources?: string[]
+  includeUrlRegex?: string
+  excludeUrlRegex?: string
+}
+
+type Enclosure = {
+  url: string
+  length: string
+  type: string
 }
 
 export type PostItem = {
-    title: string
-    link: string
-    contentSnippet?: string
-    isoDate?: string
-    dateMiliSeconds: number
+  title: string
+  link: string
+  contentSnippet?: string
+  isoDate?: string
+  dateMiliSeconds: number
+  enclosure?: Enclosure
 }
