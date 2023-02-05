a144f786aa0c20eaa8fbe4a8b5d91c699badc918
Takahiro <tkhr_621@docomo.ne.jp>
2023-02-05 17:04:50 +0900

    add domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com']
+  },
   i18n: {
     locales: ['en', 'ja'],
     defaultLocale: 'ja'
