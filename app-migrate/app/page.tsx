92e15caa5acec0c2dee65ed731701f745522e13f
Takahiro <tkhr_621@docomo.ne.jp>
2023-07-27 23:42:08 +0900

    personal   を表示

--- app-migrate/app/page.tsx
+++ app-migrate/app/page.tsx
@@ -1,5 +1,13 @@
+import { Personal } from './Personal'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <section className="text-gray-800 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col w-full mb-20">
+          <Personal />
+        </div>
+      </div>
+    </section>
   )
 }
