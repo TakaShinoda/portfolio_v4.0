8b3390ddd908666d977063a11558915ee5e45d8b
Takahiro <tkhr_621@docomo.ne.jp>
2023-11-25 02:03:53 +0900

    ブログ用の型定義を追加

--- types/types.ts
+++ types/types.ts
@@ -1,13 +1,21 @@
 export type Sites = {
-    sources?: string[]
-    includeUrlRegex?: string
-    excludeUrlRegex?: string
+  sources?: string[]
+  includeUrlRegex?: string
+  excludeUrlRegex?: string
 }
 
 export type PostItem = {
-    title: string
-    link: string
-    contentSnippet?: string
-    isoDate?: string
-    dateMiliSeconds: number
+  title: string
+  link: string
+  contentSnippet?: string
+  isoDate?: string
+  dateMiliSeconds: number
+}
+
+type Protocol = `https` | `http`
+
+export type Blog = {
+  title: string,
+  date: string,
+  url: `${Protocol}://${string}`,
 }
