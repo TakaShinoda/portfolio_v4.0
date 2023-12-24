14b6858cac56fa7babbfbb78e0d3072960f4fcb0
Takahiro <tkhr_621@docomo.ne.jp>
2023-12-25 00:39:34 +0900

    add TranslateButton

--- components/TranslateButton.tsx
+++ components/TranslateButton.tsx
@@ -1,19 +1,17 @@
+'use client'
+
 import React, { FC } from 'react'
 import { Translate } from 'iconoir-react'
-// import { useLocale } from '../hooks/useLocale'
 
 export const TranslateButton: FC = () => {
-  // const { locale } = useLocale()
-  // const isEn = locale === 'en' ? true : false
-  const isEn = false
-
   const changeLocale = () => {
-    location.href = isEn ? '/' : 'en'
+    const isEn = window.location.pathname === '/en'
+    location.href = isEn ? 'ja' : 'en'
   }
 
   return (
     <>
-      <button aria-label="DarkModeToggle" type="button" onClick={changeLocale}>
+      <button type="button" onClick={changeLocale}>
         <Translate />
       </button>
     </>
