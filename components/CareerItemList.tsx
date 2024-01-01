17a536ec755ac5b645576a22d7c90d4b61a56f3b
Takahiro <tkhr_621@docomo.ne.jp>
2024-01-01 14:35:11 +0900

    キャリアデータを切り出す

--- components/CareerItemList.tsx
+++ components/CareerItemList.tsx
@@ -1,25 +1,18 @@
 import React, { FC } from 'react'
 import { CareerItem } from './CareerItem'
+import { careerData } from '../data/career'
 
 export const CareerItemList: FC = () => {
   return (
     <ul className="list-none m-0 p-0" data-testid="career-item-list">
-      <CareerItem career="弁護士ドットコム株式会社" from="2021/11" />
-      <CareerItem
-        career="飲食系のベンチャー企業"
-        from="2020/4"
-        to="2021/10"
-      />
-      <CareerItem
-        career="宮崎大学大学院 工学研究科 工学専攻 機械・情報系コース"
-        from="2018/4"
-        to="2020/3"
-      />
-      <CareerItem
-        career="宮崎大学 工学部 情報システム工学科"
-        from="2014/4"
-        to="2018/3"
-      />
+      {careerData.map((career, i) => (
+        <CareerItem
+          key={i}
+          name={career.name}
+          from={career.from}
+          to={career.to}
+        />
+      ))}
     </ul>
   )
 }
