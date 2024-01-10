44c2da751ddfcccb69a6a57f738e555a82a48a19
Takahiro <tkhr_621@docomo.ne.jp>
2024-01-11 04:15:41 +0900

    ルールを追加

--- npmpackagejsonlint.config.js
+++ npmpackagejsonlint.config.js
@@ -1,6 +1,10 @@
 const config = {
   rules: {
     'prefer-alphabetical-scripts': 'error',
+    'prefer-no-version-zero-dependencies': 'error',
+    'prefer-no-version-zero-devDependencies': 'error',
+    'prefer-absolute-version-dependencies': 'error',
+    'prefer-absolute-version-devDependencies': 'error'
   }
 }
 
