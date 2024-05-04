50fb23f9a2968a9cc08dc4ea57dfef9e69f68c02
Takahiro <tkhr_621@docomo.ne.jp>
2024-05-05 01:52:39 +0900

    Twitter を X のアイコンに変更

--- components/Personal.tsx
+++ components/Personal.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { Building, Github, MapPin, Twitter } from 'iconoir-react'
+import { Building, Github, MapPin, X } from 'iconoir-react'
 import { Avatar, AvatarImage, AvatarFallback } from './ui/Avatar'
 import { getI18n } from '../locales/server'
 
@@ -50,7 +50,7 @@ export const Personal: FC = async () => {
           className="group inline-flex items-center text-lg font-medium pl-2 pr-3 py-2 text-gray-800 dark:text-white"
           aria-label="link to twitter"
         >
-          <Twitter />
+          <X />
         </a>
       </div>
     </div>
